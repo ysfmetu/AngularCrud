890b8553f047e646576c03de5f2b080dda41e8f8
ysfmetu <ysf_metu@hotmail.com>
2022-01-26 18:44:02 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './models/employee';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanagerapp';
+export class AppComponent implements OnInit {
+  public employees:Employee[];
+
+  constructor(private employeeService:EmployeeService){}
+
+  ngOnInit(): void {
+  //  this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+        console.log(this.employees);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
